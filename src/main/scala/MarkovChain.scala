55f4d7b6d8aa998b7f12cf9c8df3dddda22df3cc
Matt Roberts <matt.roberts2@digital.hmrc.gov.uk>
2017-08-21 10:37:38 +0100

    WIP - TODO replace list of lists with a Gen for the seed/default key (PLA-1442)

--- src/main/scala/MarkovChain.scala
+++ src/main/scala/MarkovChain.scala
@@ -5,6 +5,10 @@ import org.scalacheck.Gen._
 
 class MarkovChain[A](val data: Seq[A], val windowSize: Int) {
 
+
+  // TODO replace the idea of using a map of maps for each windowsize
+  // with a seed "last" derived from a Gen made from the data
+
   private val multimap: Map[Int, Map[Seq[A], Gen[A]]] = mapOfMaps
 
   private def mapOfMaps: Map[Int, Map[Seq[A], Gen[A]]] = {
@@ -14,8 +18,8 @@ class MarkovChain[A](val data: Seq[A], val windowSize: Int) {
   private def markov(ws: Int): Map[Seq[A], Gen[A]] = {
     data.view.iterator.sliding(ws + 1).withPartial(false)
       .map(x => (x.init, Seq(x.last))).toList.groupBy(_._1)
-      .mapValues(v => frequency(v.flatMap(_._2)
-        .groupBy(identity).mapValues(_.size).toSeq.map(x => (x._2, const(x._1))): _*))
+      .mapValues(v =>
+        frequency(v.flatMap(_._2).groupBy(identity).mapValues(_.size).toSeq.map(x => (x._2, const(x._1))): _*))
   }
 
   def next(last: Seq[A] = multimap(1).keySet.head): A = {
