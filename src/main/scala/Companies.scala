80e41466cdc9da7ec2c52e6de3772ab35c32f78a
Matt Roberts <matt.roberts2@digital.hmrc.gov.uk>
2017-08-21 17:26:56 +0100

    Reverted to map of maps as it works better. Tested with a large corpus (Dostoevsky's Crime and Punishment) and although initialisation was slow getting new company names was quick. There may be some more finessing we can do but I think the results are usable right now. (PLA-1442)

--- src/main/scala/Companies.scala
+++ src/main/scala/Companies.scala
@@ -18,8 +18,9 @@ object Companies extends Loader {
 
   val resource: URL = this.getClass.getResource("company-names.txt")
   val data: Seq[Char] = scala.io.Source.fromURL(resource).mkString.toList
-  val windowSize = 2
-  val markovChain = new MarkovChain[Char](data, windowSize)
+  val windowSize = 3
+  val terminus: List[Char] = " ".toList
+  val markovChain = new MarkovChain[Char](data, windowSize, terminus)
   lazy val _company: Gen[String] = oneOf(companies)
 
   private lazy val random = new Random
@@ -27,13 +28,12 @@ object Companies extends Loader {
   private def buildR(length: Int): String = {
 
     def innerBuild(l: Int, built: mutable.StringBuilder = mutable.StringBuilder.newBuilder): String = {
-
       if (l <= built.length) {
         built.toString()
       } else {
         built match {
-          case a if a.length > windowSize => built.append(markovChain.next(built.takeRight(windowSize).toString.toList))
-          case b if b.length > 0 => built.append(markovChain.next(built.toString.toList))
+          case a if a.length >= windowSize => built.append(markovChain.next(built.takeRight(windowSize).toString.toList))
+          case b if b.nonEmpty => built.append(markovChain.next(built.toString.toList))
           case _ => built.append(markovChain.next())
         }
         innerBuild(l, built)
@@ -42,43 +42,40 @@ object Companies extends Loader {
     innerBuild(length)
   }
 
-
   private def maybeAmpersand(companyName: String):String = companyName match {
     case c if c.split(" ").length == 2 && (random nextInt 10) < 5 => c.replace(" ", " & ")
     case _ => companyName
   }
 
-
-  // TODO check the correct way to get rid of magic number warnings
   private def maybeLtdGroupInt(companyName: String): String = {
-    val foo = 1
-    val bar = 20
+    val one = 1
+    val twenty = 20
     companyName match {
-      case c if random.nextInt(bar) < foo => c.concat(" Ltd.")
-      case c if random.nextInt(bar) < foo => c.concat(" Group")
-      case c if random.nextInt(bar) < foo => c.concat(" Inc.")
+      case c if random.nextInt(twenty) < one => c.concat(" Ltd.")
+      case c if random.nextInt(twenty) < one => c.concat(" Group")
+      case c if random.nextInt(twenty) < one => c.concat(" Inc.")
+      case c if random.nextInt(twenty) < one => c.concat(" Plc.")
+      case c if random.nextInt(twenty) < one => c.concat(" Holdings")
       case _ => companyName
     }
   }
 
-  private def removeSingleCharWords(companyName: String): String = {
-    companyName.split(" ").filter(_.length > 1).map(_.capitalize).mkString(" ") // todo should capitalise here maybe
+  private def removeShortWordsAndCapitalise(companyName: String, minLength: Int = 2): String = {
+    companyName.split(" ").filter(_.length > minLength).map(_.capitalize).mkString(" ")
   }
 
-  // not convinced we should finesse at all
   private def finesse(companyName: String): String = {
-    maybeLtdGroupInt(maybeAmpersand(removeSingleCharWords(companyName)))
+    maybeLtdGroupInt(maybeAmpersand(removeShortWordsAndCapitalise(companyName, 3)))
   }
 
   private def buildCompanyName: String = {
     val randomLength = 15
-    buildR(5 + random.nextInt(randomLength)).trim
-//    finesse(buildR(7 + random.nextInt(randomLength)).trim)
+    finesse(buildR(7 + random.nextInt(randomLength)).trim)
   }
 
   private def companies: Seq[String] = {
     val lb = new ListBuffer[String]()
-    for (i <- 1 until 750) {
+    for (i <- 1 until 1000) {
       lb += buildCompanyName
     }
     lb.toList
--- src/main/scala/MarkovChain.scala
+++ src/main/scala/MarkovChain.scala
@@ -3,16 +3,19 @@ package hmrc.smartstub
 import org.scalacheck.Gen
 import org.scalacheck.Gen._
 
-class MarkovChain[A](val data: Seq[A], val windowSize: Int) {
-
-
-  // TODO replace the idea of using a map of maps for each windowsize
-  // with a seed "last" derived from a Gen made from the data
+class MarkovChain[A](val data: Seq[A], val windowSize: Int, val terminus: Seq[A] = Seq.empty[A]) {
 
   private val multimap: Map[Int, Map[Seq[A], Gen[A]]] = mapOfMaps
 
   private def mapOfMaps: Map[Int, Map[Seq[A], Gen[A]]] = {
     (for (i <- 1 to windowSize) yield i -> markov(i)).toMap
+  }
+
+  private def start:Seq[A] = {
+    terminus match {
+      case a if a.isEmpty => multimap(1).keySet.head
+      case _ => terminus
+    }
   }
 
   private def markov(ws: Int): Map[Seq[A], Gen[A]] = {
@@ -22,9 +25,8 @@ class MarkovChain[A](val data: Seq[A], val windowSize: Int) {
         frequency(v.flatMap(_._2).groupBy(identity).mapValues(_.size).toSeq.map(x => (x._2, const(x._1))): _*))
   }
 
-  def next(last: Seq[A] = multimap(1).keySet.head): A = {
-    val gen: Gen[A] = multimap(last.size)(last)
-    gen.sample.get
+  def next(last: Seq[A] = start): A = {
+    multimap(last.size)(last).sample.get
   }
 
 }
