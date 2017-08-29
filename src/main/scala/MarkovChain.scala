0e4c1616fc4cbcd3a61b5944b2087e869a97fbcc
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2017-08-29 14:24:56 +0100

    PLA-1422 - Minor code tidy

--- src/main/scala/MarkovChain.scala
+++ src/main/scala/MarkovChain.scala
@@ -4,6 +4,7 @@ import org.scalacheck.Gen
 import org.scalacheck.Gen._
 import cats.implicits._
 import org.scalacheck.support.cats._
+import java.util.NoSuchElementException
 
 /* 
  * A MarkovChain implementation that will return a plausible/probable
@@ -15,7 +16,11 @@ import org.scalacheck.support.cats._
  * If a terminus is supplied it will use this as seed whenever an
  * empty seed is supplied.
  */
-class MarkovChain[A](val data: Seq[A], val windowSize: Int, val terminus: Seq[A] = Seq.empty[A]) {
+class MarkovChain[A](
+  val data: Seq[A],
+  val windowSize: Int,
+  val terminus: Seq[A] = Nil
+) {
 
   val multimap: Map[Int, Map[Seq[A], Gen[A]]] = {
     (for (i <- 1 to windowSize) yield i -> markov(i)).toMap
@@ -37,37 +42,26 @@ class MarkovChain[A](val data: Seq[A], val windowSize: Int, val terminus: Seq[A]
 
   private def markov(ws: Int): Map[Seq[A], Gen[A]] = {
     // slide over the given data sequence creating a sequence of sub-sequences
-    data.view.iterator.sliding(ws + 1).withPartial(false)
+    data.view.iterator.sliding(ws + 1).withPartial(false).
       // map the preceding and last items in each sub-sequence as K -> V
-      .map(x => (x.init, Seq(x.last))).toList.groupBy(_._1)
-      // create a Gen to associate with the key that knows the weighted probability of each value
-      .mapValues(v =>
-        frequency(v.flatMap(_._2).groupBy(identity).mapValues(_.size).toSeq.map(x => (x._2, const(x._1))): _*))
+      map(x => (x.init, Seq(x.last))).toList.groupBy(_._1).
+      // create a Gen to associate with the key that knows the
+      // weighted probability of each value
+      mapValues{v =>
+        frequency(v.flatMap(_._2).groupBy(identity).
+          mapValues(_.size).toSeq.map(x => (x._2, const(x._1))): _*)}
   }
 
-  private def trimSeed(seed: Seq[A]): Seq[A] = {
-    if (seed.length > windowSize) {
-      seed.takeRight(windowSize)
-    } else {
-      seed
-    }
-  }
+  private def trimSeed(seed: Seq[A]): Seq[A] = 
+    if (seed.length > windowSize) seed.takeRight(windowSize) else seed
 
   def next(seed: Seq[A] = start): Gen[A] = {
     val last = trimSeed(seed)
     try {
       multimap(last.size)(last)
     } catch {
-      case e: java.util.NoSuchElementException => throw new UnknownMarkovSeedException(e, last.mkString(", "))
+      case e: NoSuchElementException =>
+        throw new NoSuchElementException(s"$seed not found in original data.")
     }
-  }
-}
-
-class UnknownMarkovSeedException(exception: Throwable, seed: String) extends
-        RuntimeException(UnknownMarkovSeedException.message(seed), exception)
-
-object UnknownMarkovSeedException {
-  def message(seed: String): String = {
-    s"$seed not found in original data."
   }
 }
