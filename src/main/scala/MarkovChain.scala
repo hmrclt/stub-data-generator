a13adc4cb5bcb2796371c67605e132e708fb8f2f
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2017-09-08 17:13:38 +0100

    Amend based on Andys feedback

--- src/main/scala/MarkovChain.scala
+++ src/main/scala/MarkovChain.scala
@@ -4,15 +4,14 @@ import org.scalacheck.Gen
 import org.scalacheck.Gen._
 import cats.implicits._
 import org.scalacheck.support.cats._
-import java.util.NoSuchElementException
 
-/* 
+/*
  * A MarkovChain implementation that will return a plausible/probable
  * next item given a seed sub-sequence from the original data. Ideally
  * the seed should be windowSize length but it will return something
  * based on any seed (including empty) as long as the non-empty seed
- * exists in the original data. 
- * 
+ * exists in the original data.
+ *
  * If a terminus is supplied it will use this as seed whenever an
  * empty seed is supplied.
  */
@@ -22,46 +21,53 @@ class MarkovChain[A](
   val terminus: Seq[A] = Nil
 ) {
 
-  val multimap: Map[Int, Map[Seq[A], Gen[A]]] = {
-    (for (i <- 1 to windowSize) yield i -> markov(i)).toMap
-  }
+  private val multimap: Map[Int, Map[Seq[A], Gen[A]]] =
+    (1 to windowSize).map(i => i -> markov(i)).toMap
 
-  private def start:Seq[A] = {
-    if (terminus.nonEmpty) {
-      terminus
-    } else {
+  private val start: Seq[A] =
+    terminus.some.filter(_.nonEmpty).getOrElse {
       multimap(1).keySet.head
     }
-  }
 
-  def sized(numElems: Int): Gen[List[A]] = {
-    repeatM(next().map(_.pure[List]), {
-      acc: List[A] => next(trimSeed(acc)).map { acc :+ _} 
-    }, numElems)
-  }
+  def sized(numElems: Int): Gen[Vector[A]] = {
+    repeatM[Gen,(Vector[A],Option[A])](
+      next().map{a => (a.toVector,a)} , {
+        case (acc, l) => l match {
+          case Some(_) =>
+            next(trimSeed(acc)).map {
+              a => (acc ++ a.toVector, a)
+            }
+          case None => (acc,l)
+        }
+      },
+      numElems
+    )
+  }.map(_._1)
 
   private def markov(ws: Int): Map[Seq[A], Gen[A]] = {
-    // slide over the given data sequence creating a sequence of sub-sequences
-    data.view.iterator.sliding(ws + 1).withPartial(false).
-      // map the preceding and last items in each sub-sequence as K -> V
-      map(x => (x.init, Seq(x.last))).toList.groupBy(_._1).
-      // create a Gen to associate with the key that knows the
-      // weighted probability of each value
-      mapValues{v =>
-        frequency(v.flatMap(_._2).groupBy(identity).
-          mapValues(_.size).toSeq.map(x => (x._2, const(x._1))): _*)}
-  }
+    val subsequences: List[Seq[A]] =
+      data.view.iterator.sliding(ws + 1).withPartial(false).toList
 
-  private def trimSeed(seed: Seq[A]): Seq[A] = 
-    if (seed.length > windowSize) seed.takeRight(windowSize) else seed
+    val kvsets: Map[Seq[A],List[A]] = subsequences.map(
+      x => (x.init, x.last)
+    ).groupBy(_._1).mapValues(_.map(_._2))
 
-  def next(seed: Seq[A] = start): Gen[A] = {
-    val last = trimSeed(seed)
-    try {
-      multimap(last.size)(last)
-    } catch {
-      case e: NoSuchElementException =>
-        throw new NoSuchElementException(s"$seed not found in original data.")
+    kvsets.mapValues{v =>
+
+      val table = v.groupBy(identity).mapValues(_.size).map{
+        case (a,b) => (b,const(a))
+      }
+
+      frequency(table.toSeq: _*)
     }
   }
+
+  private def trimSeed(seed: Seq[A]): Seq[A] =
+    if (seed.length > windowSize) seed.takeRight(windowSize) else seed
+
+  def next(seed: Seq[A] = start): Gen[Option[A]] = {
+    val last = trimSeed(seed)
+    multimap.get(last.size).flatMap(_.get(last)).sequence
+  }
+
 }
